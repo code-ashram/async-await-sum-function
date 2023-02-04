37fafed19bf2541622138d3a86ef8ac969550ea9
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-02-04 23:03:31 +0300

    number generator ready

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 import './style.css'
+
+/* Сделайте функцию getNum1, которая возвращает промис, который с задержкой в 3 секунды выведет
+случайное число от 1 до 5. Сделайте также функцию getNum2, которая возвращает промис, который с
+задержкой в 5 секунд выведет случайное число от 6 до 10. Создайте async функцию, которая с помощью
+await будет дожидаться результата getNum1, затем будет дожидаться результата getNum2,
+а затем найдет сумму полученных чисел и выведет на экран. */
+
+const randomNumGenerator = (numberFrom: number = 1, numberTo: number = 10): number => {
+  const rangedNum = Math.abs(numberFrom - numberTo);
+  const numInRange = Math.round(Math.random() * rangedNum);
+  const minNum = Math.min(numberFrom, numberTo);
+  return numInRange + minNum;
+}
+
+const getNum1 = () => {
+  return setTimeout(() => {
+   return randomNumGenerator(1, 5)
+  })
+}
+
+console.log(getNum1())
